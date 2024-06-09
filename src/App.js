57f85567cda9eb51c9adec21b81aed1390a67e1b
Leanne <leanne.m.dk@gmail.com>
2024-06-09 15:55:53 -0700

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,85 @@
 import { useEffect, useState, useCallback } from 'react';
-import { extractLocations, getEvents } from './api';
+// import { extractLocations, getEvents } from './api';
 import CitySearch from './components/CitySearch';
 import EventList from './components/EventList';
 import NumberOfEvents from './components/NumberOfEvents';
 
 import './App.css';
+
+import mockData from './mock-data';
+
+// extracts the locations from the events
+const extractLocations = (events) => {
+  const extractedLocations = events.map((event) => event.location);
+  const locations = [...new Set(extractedLocations)];
+  return locations;
+};
+
+// gets events from the mockData file if the app is running locally, otherwise it will fetch the events from the Google Calendar API
+const getEvents = async () => {
+  if (window.location.href.startsWith('http://localhost')) {
+    return mockData;
+  } else {
+    const accessToken = await getAccessToken();
+    if (accessToken) {
+      const url =
+        // URL taken from Google Calendar API get HTTP Request; is this correct?
+        // added this URL to serverless.yml
+        'https://www.googleapis.com/calendar/v3/calendars/calendarId/events/eventId' +
+        '/' +
+        accessToken;
+      const response = await fetch(url);
+      const result = await response.json();
+      return result.events || null;
+    } else {
+      return null;
+    }
+  }
+};
+
+// gets the token from Google OAuth
+const getAccessToken = async () => {
+  // checks if the access token is in the local storage
+  const accessToken = localStorage.getItem('access_token');
+  if (accessToken) {
+    return accessToken;
+  } else {
+    // redirects the user to the Google OAuth URL
+    const code = new URLSearchParams(window.location.search).get('code');
+    if (code) {
+      return getToken(code);
+    } else {
+      redirectToAuthUrl();
+    }
+  }
+};
+
+// gets the token from Google OAuth using the provided code
+const getToken = async (code) => {
+  const encodeCode = encodeURIComponent(code);
+  const response = await fetch(
+    // URL taken from CF Ex 4.5 code check; is this correct?
+    `https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=${encodeCode}`
+  );
+  const { access_token } = await response.json();
+  if (access_token) {
+    localStorage.setItem('access_token', access_token);
+    return access_token;
+  } else {
+    return null;
+  }
+};
+
+// redirects the user to the Google OAuth URL
+const redirectToAuthUrl = async () => {
+  const response = await fetch(
+    // URL made up from Google API "Authorized Redirect URIs" + path from serverless.yml...is this correct?
+    // added this URL to serverless.yml
+    'https://leanneduyck.github.io/meet/{get-auth-url}'
+  );
+  const { authUrl } = await response.json();
+  window.location.href = authUrl;
+};
 
 const App = () => {
   // state to store the list of events
